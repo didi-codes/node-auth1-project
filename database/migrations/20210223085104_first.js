42a47ab1c1684b05b658a30c0ad56290b8440140
Damaris Garcia <damaris-garcia@lambastudents.com>
2021-02-23 09:00:05 -0500

    create knex schema for users in migrations folder

--- database/migrations/20210223085104_first.js
+++ database/migrations/20210223085104_first.js
@@ -1,9 +1,11 @@
-
-exports.up = function(knex) {
-    return knex.schema
-  
+exports.up = function (knex) {
+  return knex.schema.createTable('users', (tbl) => {
+    tbl.increments();
+    tbl.string('username', 128).notnullable().unique();
+    tbl.string('password', 256).notnullable();
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('users');
 };
